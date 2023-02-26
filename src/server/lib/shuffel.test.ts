a532eb2cab7f733ed6d631ed15448e39d902bd93
Sara Brodin <sara.brodin@gmail.com>
2023-02-26 15:46:45 +0100

    Add shuffel logic

--- src/server/lib/shuffel.test.ts
+++ src/server/lib/shuffel.test.ts
@@ -1,14 +1,50 @@
-import shuffle from './shuffle';
+import { shuffle, Draw } from './shuffle';
 
-test('REMOVE ME', () => {
+const validateShuffle = (shuffledList: Draw[], originalList:string[]) => {
+  const givers = shuffledList.map( x => x.giver);
+  const receivers = shuffledList.map( x => x.receiver);
+
+  // Check: Nbr of results
+  expect(shuffledList.length).toBe(originalList.length);
+
+  // Check: No giver gets themself
+  [...Array(originalList.length)].map((item, i) => expect(shuffledList[i]?.giver).not.toContain(shuffledList[i]?.receiver));
+
+  // Check: All values are present
+  [...Array(originalList.length)].map((item, i) => expect(givers).toContain(originalList[i]));
+  [...Array(originalList.length)].map((item, i) => expect(receivers).toContain(originalList[i]));
+}
+
+test('2 people - valid', () => {
+  const list = ['Sara', 'Gina'];
+  const copyList = ['Sara', 'Gina'];
+  const listLength = list.length;
+
+  const shuffledList = shuffle(list);
+
+  validateShuffle(shuffledList, copyList);
+});
+
+test('4 people - valid', () => {
+  const list = ['Sara', 'Gina', 'Emma', 'Sadie'];
+  const copyList = ['Sara', 'Gina', 'Emma', 'Sadie'];
+  const listLength = list.length;
+
+  const shuffledList = shuffle(list);
+
+  validateShuffle(shuffledList, copyList);
+});
+
+test('0 people - no shuffle', () => {
   expect(shuffle([]).length).toBe(0);
 });
 
-// TODO: implement
-test.skip('Test with 2 people', () => {
-  const list = ['Sara', 'Gina'];
+test('1 people - no shuffle', () => {
+  expect(shuffle([]).length).toBe(0);
+});
 
-  expect(shuffle(list).length).toBe(list.length);
-  expect(shuffle(list)).toContain('Sara');
-  expect(shuffle(list)).toContain('Gina');
-})
+// TODO: Implement functionallity
+test.skip('Non-unique 2 people is invalid', () => {
+  const list = ['Sara', 'Gina'];
+  expect(shuffle(list).length).toBe(0);
+});
--- src/server/lib/shuffle.ts
+++ src/server/lib/shuffle.ts
@@ -1,7 +1,32 @@
-export const shuffle = (list: string[]): string[] => {
+export interface Draw {
+    giver: string;
+    receiver: string;
+}
 
-    //
-    return [];
+export const shuffle = (list: string[]): Draw[] => {
+    const shuffledList: Draw[] = [];
+    if (list.length < 2) {
+        return shuffledList;
+    }
+
+    // 1. Pop List
+    let popped: string = list.pop()!;
+    const firstPopped = popped;
+
+    do {
+        // 2. Shuffle list and assign top
+        list.sort((a, b) => 0.5 - Math.random());
+        const receiver: string = list.pop()!;
+
+        // 3. Store paring
+        shuffledList.push({ giver: popped, receiver: receiver });
+        popped = receiver;
+    } while (list.length > 0);
+
+    // 5. Special handling for last giver
+    shuffledList.push({ giver: popped, receiver: firstPopped });
+
+    return shuffledList;
 };
 
 export default shuffle;
